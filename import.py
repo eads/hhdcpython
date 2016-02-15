4e67f4027596a9d4db8ba2aafa91df14cb3d43d3
David Eads <davideads@gmail.com>
2016-02-15 14:39:41 -0500

    skeletal importer

--- import.py
+++ import.py
@@ -1,2 +1,9 @@
+import os
+
+HHDC_ROOT = os.path.dirname(os.path.realpath(__file__))
+HHDC_DATA_FILE = os.path.join(HHDC_ROOT, 'data/data.csv')
+HHDC_OUTPUT_DIRECTORY = os.path.join(HHDC_ROOT, 'output')
+
 if __name__ == '__main__':
-    print('hello world')
+    print(HHDC_DATA_FILE)
+    print(HHDC_OUTPUT_DIRECTORY)
